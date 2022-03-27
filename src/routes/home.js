01746537c36030acf37f317042012cf9cfbd1f48
Blair Preston <blair.preston@student.awesomeinc.org>
2022-03-27 23:01:27 +0000

    above and below the fold

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React from 'react';
+import headShot from '../pictures/headshotSquare.jpeg';
+import {github} from '../pictures/Github.svg'
+import {email} from '../pictures/Email.svg'
+import {instagram} from '../pictures/Instagram.svg'
+import {phone} from '../pictures/Phone.svg'
+
 
 export default function Home() {
   return (
-    <div>home page</div>
+    <>
+    <div className="spacer"></div>
+    <div className="homePageContainer">
+      <div className="homeLeft">
+        <h1 className="homePageHeader">Blair Preston</h1>
+        <p className="homePageSubheader">I am in the process of becoming a </p>
+        <p className="homePageSubheader">Full-Stack Web Developer.</p>
+        <div className="svgBlock">
+          <a href="mailto:blair.preston99@gmail.com"><img src={email} width="60px" height="60px" /></a>
+          <a href="https://github.com/blair-preston"><img src={github} width="60px" height="60px" /></a>
+          <a href="tel:+8596094196"><img src={phone} width="60px" height="60px" /></a>
+          <a href="https://www.instagram.com/blair_that_music/"><img src={instagram} width="60px" height="60px" /></a>
+        </div>
+      </div>
+      <div className="homeRight">
+        <img className="headShot" src={headShot}></img>
+      </div>
+    </div>
+        <h3 className="recentProjects" >Recent Projects.</h3>
+      <div className="belowTheFold">
+        <div className="projectDisplayHome">
+          <img src="#"></img>
+          <h4 className="projectTitle" >Project name</h4>
+          <p className="projectDescription" >Description </p>
+        </div>
+        <div className="projectDisplayHome">
+          <img src="#"></img>
+          <h4 className="projectTitle">Project name</h4>
+          <p className="projectDescription" >Description </p>
+        </div>
+        <div className="projectDisplayHome">
+          <img src="#"></img>
+          <h4 className="projectTitle">Project name</h4>
+          <p className="projectDescription" >Description </p>
+        </div>
+      </div>
+    </>
   )
 }
---
+++ src/todos.md
@@ -0,0 +1,3 @@
+to do: 
+- fix on hover of nav links so they don't move
+- svg links working?
