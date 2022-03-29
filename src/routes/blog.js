34133efa9475398e606fd73421f2dae4e62d784d
Blair Preston <blair.preston@student.awesomeinc.org>
2022-03-29 01:57:53 +0000

    update blog page

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 export default function Blog() {
   return (
-    <div>Blog page</div>
+    <div className="blogPageContainer" >
+        <h1 className="aboutPageHeader">Blog.</h1>
+        <h4 className="blogHeader">BLOG TITLE</h4>
+        <p className="blogTeaserText" >Et velit quis est explicabo assumenda et unde suscipit. 
+         Vel nobis galisum qui necessitatibus minima nam maxime voluptates et minus praesentium ut voluptas consequatur.</p>
+        <a className="blogReadMore" href="#">Read more</a>
+        <h4 className="blogHeader">BLOG TITLE</h4>
+        <p className="blogTeaserText" >Et velit quis est explicabo assumenda et unde suscipit. 
+         Vel nobis galisum qui necessitatibus minima nam maxime voluptates et minus praesentium ut voluptas consequatur.</p>
+        <a className="blogReadMore" href="#">Read more</a>
+        <h4 className="blogHeader">BLOG TITLE</h4>
+        <p className="blogTeaserText" >Et velit quis est explicabo assumenda et unde suscipit. 
+         Vel nobis galisum qui necessitatibus minima nam maxime voluptates et minus praesentium ut voluptas consequatur.</p>
+        <a className="blogReadMore" href="#">Read more</a>
+        <h4 className="blogHeader">BLOG TITLE</h4>
+        <p className="blogTeaserText" >Et velit quis est explicabo assumenda et unde suscipit. 
+         Vel nobis galisum qui necessitatibus minima nam maxime voluptates et minus praesentium ut voluptas consequatur.</p>
+        <a className="blogReadMore" href="#">Read more</a>
+        <h4 className="blogHeader">BLOG TITLE</h4>
+        <p className="blogTeaserText" >Et velit quis est explicabo assumenda et unde suscipit. 
+         Vel nobis galisum qui necessitatibus minima nam maxime voluptates et minus praesentium ut voluptas consequatur.</p>
+        <a className="blogReadMore" href="#">Read more</a>
+        
+    </div>
   )
 }
 
--- src/todos.md
+++ src/todos.md
@@ -2,3 +2,7 @@ to do:
 - fix on hover of nav links so they don't move
 - svg links working?
 - make hover of nav links like the restaurant hover
+- animations
+- blog links
+- footer 
+- arrows next to read more
