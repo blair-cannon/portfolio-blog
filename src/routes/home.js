80aaaf1ce42398a8ccc52a471a04a37ea5d0cd68
Blair Preston <blair.preston@student.awesomeinc.org>
2022-05-17 11:58:31 +0000

    update final project link

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,4 +1,4 @@
-import React, {Image} from 'react';
+import React from 'react';
 import { useNavigate } from "react-router-dom";
 import headShot from '../pictures/headshotSquare.jpeg';
 import github from '../pictures/Github.svg';
@@ -27,35 +27,35 @@ export default function Home() {
         <p className="homePageSubheader">I am in the process of becoming a </p>
         <p className="homePageSubheader">Full-Stack Web Developer.</p>
         <div className="svgBlock">
-          <a className="svg" href="mailto:blair.preston99@gmail.com"><img src={email} width="60px" height="60px" /></a>
-          <a className="svg" href="https://github.com/blair-preston"><img src={github} width="60px" height="60px" /></a>
-          <a className="svg" href="tel:+8596094196"><img src={phone} width="60px" height="60px" /></a>
-          <a className="svg" href="https://www.instagram.com/blair_that_music/"><img src={instagram} width="60px" height="60px" /></a>
+          <a className="svg" href="mailto:blair.preston99@gmail.com" ><img src={email} width="60px" height="60px" alt="mail svg" /></a>
+          <a className="svg" href="https://github.com/blair-preston" ><img src={github} width="60px" height="60px" alt="github svg" /></a>
+          <a className="svg" href="tel:+8596094196"><img src={phone} width="60px" height="60px" alt="phone svg" /></a>
+          <a className="svg" href="https://www.instagram.com/blair_that_music/"><img src={instagram} width="60px" height="60px" alt="instagram svg"  /></a>
         </div>
       </div>
       <div className="homeRight">
-        <img className="headShot" src={headShot}></img>
+        <img className="headShot" src={headShot} alt="headshot"></img>
       </div>
     </div>
         <h3 className="recentProjects" >Recent Projects.</h3>
       <div className="belowTheFold">
         <div>
-            <a href="https://github.com/blair-preston/final-project-treater">
-              <img className="projectDisplayHome" src={finalproject}></img>
+            <a href="https://mud-buds.herokuapp.com/">
+              <img className="projectDisplayHome" src={finalproject} alt="final project"></img>
             </a>
           <h4 className="projectTitle" >Mud-Buds</h4>
           <p className="projectDescription" >Tinder clone with a spin! <br/> Find potential puppy playmates in your area! <br/> Django backend, React frontend. </p>
         </div>
         <div>
           <a href="https://blair-preston.github.io/react-restaurant/">
-            <img className="projectDisplayHome" src={restaurant}></img>
+            <img className="projectDisplayHome" src={restaurant} alt="restaurant project"></img>
           </a>
           <h4 className="projectTitle">React Restaurant</h4>
           <p className="projectDescription" >A sample restaurant website pulling from a database.</p>
         </div>
         <div>
           <a href="https://blair-preston.github.io/to-do-list/">
-            <img className="projectDisplayHome" src={todolist}></img>
+            <img className="projectDisplayHome" src={todolist} alt="to do list project" ></img>
           </a>
           <h4 className="projectTitle">To Do List</h4>
           <p className="projectDescription" >To-do-list webpage dynamically rendered <br/> with React Components.</p>
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -13,64 +13,64 @@ export default function Projects() {
   return (
     <div className="projectPageDiv">
       <div>
-        <a href="https://github.com/blair-preston/final-project-treater">
-          <img className="projectDisplayHome" src={finalproject}></img>
+        <a href="https://mud-buds.herokuapp.com/">
+          <img className="projectDisplayHome" src={finalproject} alt="project"></img>
         </a>
         <h4 className="projectTitle" >Mud-Buds</h4>
         <p className="projectDescription" >Tinder clone with a spin! <br/> Find potential puppy playmates in your area! <br/> Django backend, React frontend. </p>
       </div>
       <div>
         <a href="https://blair-preston.github.io/react-restaurant/">
-          <img className="projectDisplayHome" src={restaurant}></img>
+          <img className="projectDisplayHome" src={restaurant} alt="project"></img>
         </a>
         <h4 className="projectTitle">React Restaurant</h4>
         <p className="projectDescription" >A sample restaurant website pulling from a database.</p>
       </div>
       <div>
         <a href="https://blair-preston.github.io/to-do-list/">
-          <img className="projectDisplayHome" src={todolist}></img>
+          <img className="projectDisplayHome" src={todolist} alt="project"></img>
         </a>
         <h4 className="projectTitle">To Do List</h4>
         <p className="projectDescription" >To-do-list webpage dynamically rendered <br/> with React Components.</p>
       </div>
       <div >
         <a href="https://blair-preston.github.io/tic-tac-toe/">
-          <img className="projectDisplayHome" src={tictactoe}></img>
+          <img className="projectDisplayHome" src={tictactoe} alt="project"></img>
         </a>
         <h4 className="projectTitle" >Tic-Tac-Toe</h4>
         <p className="projectDescription" >Classic game implemented with Object <br/> Oriented Programming and a MVC framework.</p>
       </div>
       <div >
         <a href="https://blair-preston.github.io/weather-app/">
-          <img className="projectDisplayHome" src={weatherapp}></img>
+          <img className="projectDisplayHome" src={weatherapp} alt="project"></img>
         </a>
         <h4 className="projectTitle">Weather App</h4>
         <p className="projectDescription" >Displays current weather information using Axios to fetch <br/> data and utilizing local storage to require less API requests. </p>
       </div>
       <div >
         <a href="https://github.com/blair-preston/django_to_do_list">
-          <img className="projectDisplayHome" src={backendtodolist}></img>
+          <img className="projectDisplayHome" src={backendtodolist} alt="project"></img>
         </a>
         <h4 className="projectTitle">Django To Do List</h4>
         <p className="projectDescription" >An API that saves a user’s to-do list items in a <br/> PostgresQL database. </p>
       </div>
       <div >
         <a href="https://blair-preston.github.io/bouncing-ball/">
-          <img className="projectDisplayHome" src={bouncingballs}></img>
+          <img className="projectDisplayHome" src={bouncingballs} alt="project"></img>
         </a>
         <h4 className="projectTitle">Bouncing Balls Game</h4>
         <p className="projectDescription" >Interactive, two-player game created with <br/> Object-Oriented-Programming. </p>
       </div>
       <div >
         <a href="https://github.com/blair-preston/python-evercraft">
-          <img className="projectDisplayHome" src={evercraft}></img>
+          <img className="projectDisplayHome" src={evercraft} alt="project"></img>
         </a>
         <h4 className="projectTitle">Evercraft</h4>
         <p className="projectDescription" >Developing an RPG-Evercraft backend using <br/> Test Driven Development. </p>
       </div>
       <div >
         <a href="https://blair-preston.github.io/alarmclock/">
-          <img className="projectDisplayHome" src={alarmclock}></img>
+          <img className="projectDisplayHome" src={alarmclock} alt="project"></img>
         </a>
         <h4 className="projectTitle">Alarm Clock</h4>
         <p className="projectDescription" >Use of the Date object, setInterval, and setTimeout <br/> to display a digital alarm clock. </p>
