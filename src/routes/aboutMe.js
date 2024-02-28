4dfb13fc5006cc97bf8c7309110fa8db0fe7cb83
Blair Preston <blair.preston99@gmail.com>
2024-02-28 15:34:44 +0000

    update about me page for up to date relevance

--- src/routes/aboutMe.js
+++ src/routes/aboutMe.js
@@ -5,20 +5,17 @@ export default function AboutMe() {
   return (
     <div className="aboutPageContainer" >
       <div className="aboutImageWrapper">
-        <img className="aboutImage" src={aboutImage} alt="about me"></img>
+       <img className="aboutImage" src={aboutImage} alt="about me"></img> 
       </div>
         <h1 className="aboutPageHeader">About me.</h1>
       <div className="aboutMeBioBox">
         <p className="aboutMeBio">
-          Hi there! I'm Blair! I have been pursuing a passion project in web development since January 2022 where I have learned an array of 
-          front-end and back-end languages. Thinking through a problem, making a plan for solving it and then translating that 
-          into working code has been one of the most rewarding processes! I'm excited and determined to keep this journey alive! <br/> <br/>
-
-          More about me. <br/>
-          In May of 2021, I graduated from UK and began applying to medical schools as I had always planned on. I started to notice
+          In May of 2021, I graduated from the University of Kentucky and began applying to medical schools as I had always planned on. I started to notice
           a growing itch inside of me to want a fresh, broadened path for my future. Long story short, I took a shot in the dark and
-          signed up for Awesome Inc's 16-week, intensive training program for aspiring software developers. It has been the 
-          best thing that has happened for my life. I already know that this knowledge will bring so much fruit into my life. <br/> 
+          signed up for Awesome Inc's 16-week, intensive training program for aspiring software developers and haven't looked back. It was an amazing journey and
+          provided the high-paced, challenging envirnoment that I enjoyed from undergrad. My job with Covetrus shortly followed and has been another great environment where
+          I have grown tremendously. I have found that my strengths in learning have translated seemlessly into this field and I am eager to continue pushing myself.
+          Excited to see what other fruit is in store on this path!<br/> 
         </p>
       </div>
     </div>
